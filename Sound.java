eb3da4a479d520354283fbcdae8cce268cda3592
recrudescence <calvinmwang@gmail.com>
2014-05-19 15:18:21 -0400

    minor changes to which hauntingly beautiful tracks to use

--- Sound.java
+++ Sound.java
@@ -4,10 +4,9 @@ import java.applet.Applet;
 import java.applet.AudioClip;
 
 public class Sound {
+	// we use finals so that after they initialize, they don't need to be loaded again.
 	public static final AudioClip BOOP = Applet.newAudioClip(Sound.class.getResource("sounds/boop.wav"));
 	public static final AudioClip BEEP = Applet.newAudioClip(Sound.class.getResource("sounds/beep.wav"));
-	public static final AudioClip DEATH1 = Applet.newAudioClip(Sound.class.getResource("sounds/eh.wav"));
-	public static final AudioClip DEATH2 = Applet.newAudioClip(Sound.class.getResource("sounds/gameover.wav"));
+	public static final AudioClip DEATH = Applet.newAudioClip(Sound.class.getResource("sounds/gameover.wav"));
 	public static final AudioClip BG1 = Applet.newAudioClip(Sound.class.getResource("sounds/dundundun.wav"));
-	//public static final AudioClip BG2 = Applet.newAudioClip(Sound.class.getResource("sounds/bopbopbop.wav"));
 }
